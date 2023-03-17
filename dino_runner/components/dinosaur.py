49b325d48cf29bdc1f99fde9e990e0b189fd36c2
Luiz Paulo <luiz.lira029@gmail.com>
2023-03-17 19:57:03 -0300

    AULA 2: Added obstacle

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,14 @@
 import pygame
+from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
-class Dinosaur:
+
+
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -28,12 +31,16 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_jump = False
+            self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+            self.dino_duck = False
         
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
         
 
         if self.step_index >= 10:
