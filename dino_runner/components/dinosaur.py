8238455560ee128ff256f9042a61fede9ec5dd00
Luiz Paulo <luiz.lira029@gmail.com>
2023-03-17 11:18:30 -0300

    feat: implemented duck method in Dinosaur class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
@@ -15,12 +15,15 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_jump = False
         self.dino_run = True
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
         if self.dino_jump:
             self.jump()
+        if self.dino_duck:
+            self.duck()
         
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -28,6 +31,10 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+        
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+        
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -54,4 +61,9 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = 340
+        self.step_index += 1
+        self.dino_duck = False
